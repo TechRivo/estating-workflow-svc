22b2c505393a15d252aa44bcebac8e4bfa6f34d1
patrykmajchrzycki <patryk.majchrzycki@gmail.com>
2023-08-22 18:32:12 +0200

    #862k8xm80 Initialise Workflow Service

---
+++ src/main/java/com/estating/workflow/WorkflowSvcApplication.java
@@ -0,0 +1,13 @@
+package com.estating.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estating/workflow/WorkflowSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estating.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
